c97552f04795f3261aa0804f2a3eeb5192272e7d
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-09-12 12:30:40 -0700

    Completed reverse_words challenge

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,31 @@
 # A method to reverse each word in a sentence, in place.
-def reverse_words(my_words)
-  raise NotImplementedError
+# Each word will need its letters reversed
+def reverse_words(str)
+  # return nil if str.nil?
+  return nil if str.nil?
+  # Return empty string if length is zero
+  return "" if str.length == 0
+
+  # Empty string
+  rev_str = ""
+  i = -1
+  while str[i] != nil
+    rev_str += str[i]
+    i = i - 1
+  end
+  return rev_str
 end
+
+# Time = O(n) - Goes through entire array
+# Space - O(n) - We're creating another array in memory 
+
+test_string = "Engineer"
+# p reverse_words(nil)
+
+p reverse_words(test_string)
+
+# Time Complexity
+# Is going through half the array, depends on input length O(n)
+
+# Space Complexity
+# Is reversing in place/always will use the same number of variables = O(1)
