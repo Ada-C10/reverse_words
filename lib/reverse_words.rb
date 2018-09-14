5519c417a36e03fba7d4919b747a003bf686aee8
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-09-13 19:45:00 -0700

    solved reverse words in string, tests passed

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,35 @@
 # A method to reverse each word in a sentence, in place.
+
+#index set to 0, loop through until you get to a space
+#once you're on a space record that index and and set your second index to the index of that space minus one
+# then begin looping through the word to reverse the letters
+require 'pry'
+
+
 def reverse_words(my_words)
-  raise NotImplementedError
+  return nil if my_words == nil
+
+  i = 0
+  j = 0
+  until my_words[i-1] == nil
+
+    if my_words[i] == " " || my_words[i] == nil
+
+      m = j
+      k = (i - 1)
+      while m < k
+        temp = my_words[m]
+        my_words[m] = my_words[k]
+        my_words[k] = temp
+        m += 1
+        k -= 1
+      end
+      j = i + 1
+
+    end
+    i += 1
+
+  end
+
+  return my_words
 end
