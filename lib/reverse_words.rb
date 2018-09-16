3cb9bad69e46ec521a5598e459d1be48b9df56db
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-09-15 23:04:00 -0700

    reverse words methods built, prbly need refinement but do pass all tests currently

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,69 +1,31 @@
 require 'pry'
 def reverse_words(my_words)
   if !my_words.nil? && !my_words.empty?
-    n = my_words.length - 1
-    i = 0
-    while i <= n
-      if my_words[i] == ' ' || i == n
-        n = i
-        reverse_single_word(i, n, my_words)
+    full_length = my_words.length
+    index = 0
+    iter = 0
+    while index <= full_length
+      if my_words[index] == ' ' || index == full_length
+        word_end = index
+        reverse_single_word(iter, word_end, my_words)
+        iter = index + 1
       end
-      i += 1
+      index += 1
     end
   end
   return my_words
 end
 
+def reverse_single_word(index, word_end, my_words)
 
-def reverse_single_word(i, n, my_words)
-  i -= n
-  while i < n
-    temp = my_words[i]
-    my_words[i] = my_words[n]
-    my_words[n] = temp
-    i += 1
-    n -= 1
+  while index < word_end
+    temp = my_words[index]
+    my_words[index] = my_words[word_end - 1]
+    my_words[word_end - 1] = temp
+    index += 1
+    word_end -= 1
   end
   return my_words
 end
 
 
-
-# # A method to reverse each word in a sentence, in place.
-# def reverse_words(my_words)
-#   # if !my_words.nil? && !my_words.empty?
-#     # get length of all words combined
-#     len_arr = my_words.length
-#     # set iterator
-#     iterator = 0
-#     i = 0
-#     # binding.pry
-#     # iterate until '' encountered
-#     while iterator <= len_arr
-#       if my_words[iterator] == '' || iterator == len_arr
-#         spc_val = iterator
-#         binding.pry
-#         # pass iterator value, found space val, and my_words to helper method
-#         reverse_a_word(i, spc_val, my_words)
-#       end
-#       iterator += 1
-#       i += 1
-#     end
-#     # binding.pry
-#     return my_words
-#     # else return nil
-#   # end
-# end
-
-
-# def reverse_words(my_words)
-#   while i < (spc_val - 1)
-#     temp = my_words[i]
-#     my_words[i] = my_words[spc_val - 1]
-#     my_words[spc_val - 1] = temp
-#     i += 1
-#     spc_val -= 1
-#   end
-#   return my_words
-#
-# end
