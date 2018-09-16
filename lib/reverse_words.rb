56ee677669ef3818dd9381dd3ae0d3ea5f39e654
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-16 10:43:52 -0700

    Completed reverse words exercise. Added reporting line to reverse_words_spec.rb to see report of passed tests.

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,36 @@
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  raise NotImplementedError
+  if my_words == nil
+    return nil
+  elsif my_words == ""
+    return ""
+  end
+
+  index = 0
+  while my_words[index] != nil
+
+    if my_words[index] == " "
+      index += 1
+      next
+    end
+
+    count = 0
+    while my_words[index] != " " && my_words[index] != nil
+      count += 1
+      index += 1
+    end
+
+    beg_index = index - count
+    end_index = index - 1
+    temp = ""
+    while beg_index < end_index
+      temp = my_words[beg_index]
+      my_words[beg_index] = my_words[end_index]
+      my_words[end_index] = temp
+      beg_index += 1
+      end_index -= 1
+    end
+  end
+
+  return my_words
 end
