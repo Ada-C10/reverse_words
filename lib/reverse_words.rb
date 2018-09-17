223efc0eee56dee0c299e434f926213507b4385c
Carly Reiter <yepodacarly@gmail.com>
2018-09-16 21:32:06 -0700

    my solution appears to work, when using pry, but doesn't pass the tests.  hmmm.  I guess I'll be attending Wednesday's review session.

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,12 +1,43 @@
-# 1. Determine the starting index of a word and the index of the last character in that word (the next character will be a ' ' i.e. a space, or it would be the last character in the string)
-# 2. Call a helper method passing in the original input string, starting index and ending index of the current word.
-# 3. The helper method reverses all the characters from starting index to ending index.
+require 'pry'
 
-# A method to reverse each word in a sentence, in place.
-def reverse_words(my_words)
-  return my_words[0]
+def string_reverse(my_string)
+  if my_string == nil || my_string.length == 0
+    return my_string
+  end
+  array = my_string.split("")
+  length = array.length
+  i = 0
+  j = (length - 1)
+  holding = nil
+  while i < j
+    holding = array[i]
+    array[i] = array[j]
+    array[j] = holding
+    i += 1
+    j -= 1
+  end
+  joined_array = array.join("")
+  return joined_array
 end
 
+def reverse_words(my_words)
+  sentence = []
+  my_words.split.each do |word|
+    sentence << word
+  end
 
-example = "I wanna understand this"
-puts reverse_words(example)
+  reversed = []
+  sentence.each do |word|
+    reversed << string_reverse(word)
+  end
+
+  final = ""
+  reversed.each do |word|
+    final << word + " "
+  end
+  return final
+
+end
+
+# example = reverse_words("I strive to engineer the most efficient solutions.")
+# puts example
