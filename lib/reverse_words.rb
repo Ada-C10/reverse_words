be9c423c8a12037e37419802c2c7a9e7dfaf803c
sabine <drsabinethomas@gmail.com>
2018-09-16 18:47:56 -0700

    Tests are not passing and need to review algorithm, submitting pull request for further review

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,38 @@
+##### Created baseline to review reverse_method once Shruti has posted answer!
+
 # A method to reverse each word in a sentence, in place.
-def reverse_words(my_words)
-  raise NotImplementedError
-end
+# def reverse_words(my_words)
+#   # raise NotImplementedError
+#   return nil if my_words == nil
+#   return nil if my_words.length < 1
+#
+#   length = my_words.length
+#
+#   i = 0
+#   # while the index is less than the length of the word
+#   while i < length
+#     # find the space and determine what to do with it
+#     # assign the space to a variable introduce j
+#     # while the index is equal to a space
+#     while my_words[i] == " "
+#       # i += 1
+#       j == my_words[i]
+#     end
+    # while the index is less than j aka the space
+    # reverse the word
+    # while my_words[i] < j
+    #   # while my_words[i] == " "
+    #   #   i += 1
+    #   # end
+    #   # while my_words[j] == " "
+    #   temp = my_words[i]
+    #   my_words[i] == j
+    #   j == temp
+    #   i += 1
+    #   j -= 1
+    #   # end
+    #   i  == j + 1
+    # end
+    # return temp
+#   end
+# end # method
