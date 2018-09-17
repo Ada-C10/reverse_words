a8b75a59d2944c41ca161d08609b7a01c6bb342c
sabine <drsabinethomas@gmail.com>
2018-09-16 19:00:59 -0700

    Update with Shruti's Slack comment to be worked on

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,3 +1,16 @@
+##### From Shruti on 9/16/18
+
+# Just in case you could use a hint and didn't reach out to me yet to share your thoughts and ideas so far, or to ask for a hint. Here's the hint on *reverse words* homework assignment that is due tomorrow:
+#1. Determine the starting index of a word and the index of the last character in that word (the next character will be a ' ' i.e. a space, or it would be the last character in the string)
+#2. Call a helper method passing in the original input string, starting index and ending index of the current word.
+#3. The helper method reverses all the characters from starting index to ending index. (edited)
+
+
+
+
+
+
+
 ##### Created baseline to review reverse_method once Shruti has posted answer!
 
 # A method to reverse each word in a sentence, in place.
