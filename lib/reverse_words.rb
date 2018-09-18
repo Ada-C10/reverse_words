f504a62132460c0f8abee3b140d2aaacf4290fe9
Katricia Smith <smithkatricia@gmail.com>
2018-09-17 20:50:04 -0700

    method to reverse words

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,53 @@
+require 'pry'
 # A method to reverse each word in a sentence, in place.
+def reverse_string(my_string)
+  if my_string == nil || my_string.length <= 1
+    return my_string
+  else
+    i = 0
+    j = my_string.length - 1
+    while i < j
+      temp = my_string[i]
+      my_string[i] = my_string[j]
+      my_string[j]=temp
+      i += 1
+      j -= 1
+    end
+    return my_string
+  end
+end
+
+# # Reverse strings and array.
+# def reverse_words(my_words)
+#   if my_words.length == nil
+#     return my_words
+#   else
+#     g = 0
+#     h = (my_words.length) -1
+#     while g < h
+#       temp = my_words[h]
+#       my_words[h] = reverse_string(my_words[g])
+#       my_words[g] = reverse_string(temp)
+#       g+= 1
+#       h -= 1
+#       # binding.pry
+#     end
+#     return my_words
+#   end
+# end
+
 def reverse_words(my_words)
-  raise NotImplementedError
+  if my_words.length == nil
+    return my_words
+  else
+    i = 0
+    while i < my_words.length
+      my_words[i] = reverse_string(my_words[i])
+      i += 1
+
+    end
+
+    return my_words
+  end
+  binding.pry
 end
