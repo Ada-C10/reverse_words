488bafec4531145753d1415745bdcdf0d9b8b0c7
Maryam Shitu <maryamshitu@gmail.com>
2018-09-17 22:24:54 -0700

    errors

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,31 +1,10 @@
 # # A method to reverse each word in a sentence, in place.
 require 'pry'
 
-
-# def reverse_words(my_words)
-#   if my_words == nil
-#     return nil
-#   end
-#
-#   i = 0
-#   j = my_words.length - 1
-#
-#   while i < j
-#     first_letter = my_words[i]
-#     last_letter = my_words[j]
-#
-#     my_words[i] = last_letter
-#     my_words[j] = first_letter
-#
-#     i += 1
-#     j -= 1
-#   end
-#
-# end
-
-################################
-
 def reverse_one_word(word, index)
+  if word == nil
+     return nil
+  end
 
   i = 0
   while i < index
@@ -42,27 +21,25 @@ def reverse_one_word(word, index)
   return word
 end
 
-
 def reverse_words(my_words)
-  if my_words.nil?
+  if my_words == nil
      return nil
   end
 
-  k = 0
-  l = my_words.length
+  z = 0
+  y = my_words.length
 
-  reverse_one_word(my_words, l)
 
-    # while i < j
-    #   if my_words[i] == " " && i != 0
-    #     reverse_one_word(my_words, i)
-    #       while (i + 1) < j
-    #         reverse_one_word(my_words, j)
-    #       end
-    #     else
-    #       reverse_one_word(my_words, j)
-    #   end
-    #   i += 1
-    # end
+    while z < y
+      if my_words[z] == " " && z != 0
+        reverse_one_word(my_words, y)
+          while (z + 1) < y
+            reverse_one_word(my_words, y)
+          end
+        else
+          reverse_one_word(my_words, y)
+      end
+      z += 1
+    end
 
 end
