4f9b634a47df0d8105bc846728727b6a959c602c
Maryam Shitu <maryamshitu@gmail.com>
2018-09-18 23:39:51 -0700

    tests passing

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,12 +1,14 @@
 # # A method to reverse each word in a sentence, in place.
 require 'pry'
 
-def reverse_one_word(word, index)
+def reverse_one_word(word, start = 0, finish = word.length)
   if word == nil
-     return nil
+    return nil
   end
 
-  i = 0
+  i = start
+  index = finish
+
   while i < index
     first_letter = word[i]
     last_letter = word[index]
@@ -23,23 +25,37 @@ end
 
 def reverse_words(my_words)
   if my_words == nil
-     return nil
+    return nil
   end
 
-  z = 0
-  y = my_words.length
+  current_index = 0
 
+  start = nil
+  finish = nil
 
-    while z < y
-      if my_words[z] == " " && z != 0
-        reverse_one_word(my_words, y)
-          while (z + 1) < y
-            reverse_one_word(my_words, y)
-          end
-        else
-          reverse_one_word(my_words, y)
-      end
-      z += 1
+  while current_index < my_words.length
+    currently_on_letter = my_words[current_index] != " "
+    currently_on_space = my_words[current_index] == " "
+    at_end_of_string = current_index == my_words.length - 1
+
+    if start == nil && currently_on_letter
+      start = current_index
     end
 
+    if start && currently_on_space
+      finish = current_index - 1
+    end
+
+    if start && at_end_of_string
+      finish = current_index
+    end
+
+    if start && finish
+      reverse_one_word(my_words, start, finish)
+      start = nil
+      finish = nil
+    end
+
+    current_index += 1
+  end
 end
