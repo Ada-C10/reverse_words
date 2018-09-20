1c4ceb64e127932767fa2e5dc7b958c95ac89308
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-09-20 14:03:47 -0700

    made changes to reverse words after finding bug

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,43 +1,41 @@
 
 #Algorithm Plan:
-#starting index set to 0, loop through until you reach a space to indicate the end of a word
-#record that index as an indicator of where the word ends and take the starting index
-#using index of where word starts and ends reverse letters in place (like previous reverse word or reverse array)
-#execute until your last index count has passed the end of the array, so until i -1 == nil
+#Helper method that takes the starting index of a word, the ending index of a word and the string
+#This helper method will reverse the words in place. There will be a loop that runs until the starting
+#index becomes equal to the ending index, and each time through the loop we will use a temp value so that
+#we can set the item at one index to the other. Then increment starting index by one and descrease ending index by one.
 
-# Time complexity is O(n^2) because we have two nested loops. For each item in
-# the array we will have to loop through up to n set of other letters in the array
-# to determine word length, then we will have to loop through each letter in the
-# word (also up to n or technically n/2 but the /2 part is dropped for that loop).
-# O(n^2) makes the time complexity quadratic.
+def reverse_word(start_i, end_i, all_words)
+  while start_i < end_i
+    temp = all_words[start_i]
+    all_words[start_i] = all_words[end_i]
+    all_words[end_i] = temp
+    start_i += 1
+    end_i -= 1
+  end
+  return all_words
+end
 
-#Space complexity is O(1), or constant, because we do not need to store a new array in memory
-# no matter how long the string we only need to store the 4 variable values. 
 
+
+#https://github.com/Ada-C10/reverse_words/blob/solution/lib/reverse_words.rb
 
 def reverse_words(my_words)
-  return nil if my_words == nil
+  return if my_words == nil || my_words.length == 0
 
   i = 0
-  j = 0
-  until my_words[i-1] == nil
-
-    if my_words[i] == " "
-
-      m = j
-      k = (i - 1)
-      while m < k
-        temp = my_words[m]
-        my_words[m] = my_words[k]
-        my_words[k] = temp
-        m += 1
-        k -= 1
-      end
-      j = i + 1
-
+  while i < my_words.length
+    while my_words[i] == " " && i < my_words.length
+      i += 1
     end
-    i += 1
+    s = i
 
+    while my_words[i] != " " && i < my_words.length
+      i += 1
+    end
+    e = i - 1
+
+    reverse_word(s, e, my_words)
   end
 
   return my_words
