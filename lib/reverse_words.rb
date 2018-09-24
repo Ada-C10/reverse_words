0ef9b4fc1ea5c8d3fc1bd42237340186fc87d254
Semret Nicodimos <snicodimos@gmail.com>
2018-09-24 09:06:01 -0700

    not fully functional but some working codes with two methods

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,49 @@
 # A method to reverse each word in a sentence, in place.
+# you need to do two methods, first figuring out the start and end index of the words in the sentence.
+
+# the first method is going to the same as reverse string
+
+# second method is where we intialize the start and end index for each word
+
+# we'll pass in the first method into the second to do the swap.
+
+def string_reverse (my_string, s_index, e_index)
+  i = s_index
+  j = e_index
+
+  while i < j
+    temp = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = temp
+
+    i += 1
+    j -= 1
+  end
+
+  return my_string
+end
+
+
 def reverse_words(my_words)
-  raise NotImplementedError
+  if my_words == nil
+    return my_words
+  end
+
+  i = 0
+  j = my_words.length
+
+  while i < j # will do the while loop until the end of my_words
+    while my_words[i] == " "
+      i + 1
+    end
+    s_index = i
+
+    while my_words[i] != " "
+      i += 1
+    end
+    e_index = i - 1
+
+    string_reverse(my_words, s_index, e_index)
+  end
+  return my_words
 end
