4b054552957ad34157e1cc6bcacd74cf9ff2dc3b
Chantelle Belic <chantelle.belic@gmail.com>
2018-09-26 17:07:59 -0700

    fixed reverse_words, all tests passing with just right and left index

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -18,61 +18,32 @@ def word_reverse(my_string, right_index, left_index)
   return my_string
 end
 
-# A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
 
   return if (my_words == nil || my_words.length == 0)
 
-  counter = 0
+  right_index = 0
+  left_index = 0
 
-  while counter < my_words.length
+  while left_index < my_words.length
 
-    while my_words[counter] == ' ' && counter < my_words.length
-      counter += 1
+    while my_words[right_index] == ' ' && right_index < my_words.length
+      right_index += 1
     end
 
-    right_index = counter
+    left_index = right_index + 1
 
-    while my_words[counter]!= ' ' && counter < my_words.length
-      counter += 1
+    while my_words[left_index]!= ' ' && left_index < my_words.length
+      left_index += 1
     end
 
-    left_index = counter - 1
+    left_index -= 1
 
     word_reverse(my_words, right_index, left_index)
+
+    right_index = left_index + 1
   end
 
   return my_words
 
 end
-
-#2 failures:
-# def reverse_words(my_words)
-#
-#   return if (my_words == nil || my_words.length == 0)
-#
-#   right_index = 0
-#   left_index = 0
-#
-#   while right_index < my_words.length
-#
-#     right_index = left_index + 1
-#
-#     while my_words[right_index] == ' ' && right_index < my_words.length
-#       right_index += 1
-#     end
-#
-#     left_index = right_index
-#
-#     while my_words[left_index]!= ' ' && left_index < my_words.length
-#       left_index += 1
-#     end
-#
-#     left_index -= 1
-#
-#     word_reverse(my_words, right_index, left_index)
-#   end
-#
-#   return my_words
-#
-# end
