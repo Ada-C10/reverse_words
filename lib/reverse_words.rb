e754da330e357785c8fd71ae3a28940818f726e5
Amber Lynn <griffiam@gmail.com>
2018-09-27 22:17:21 -0700

    had trouble figuring out how to skip over spaces

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,57 @@
 # A method to reverse each word in a sentence, in place.
+# def string_reverse(my_string)
+#
+#   if my_string == nil
+#     return nil
+#   elsif my_string.length == 1
+#     return my_string
+#   elsif
+#     my_string.length > 1
+#     temp = ""
+#     i = 0
+#     j = (my_string.length - 1)
+#     while i < j
+#       temp = my_string[i]
+#       my_string[i] = my_string[j]
+#       my_string[j] = temp
+#       i += 1
+#       j -= 1
+#     end
+#     return my_string
+#   end
+# end
+
+
 def reverse_words(my_words)
-  raise NotImplementedError
+  if my_words == nil || my_words.length == 0
+    return nil
+  else
+    start = 0
+    ender = 0
+
+    my_words.length.times do |i|
+      ender = i
+
+      if my_words[i] == " "
+        ender = i - 1
+        while start < ender
+          temp = my_words[ender]
+          my_words[ender] = my_words[start]
+          my_words[start] = temp
+          start += 1
+          ender -= 1
+        end
+        start = i + 1
+      end
+    end
+
+    while start < ender
+      temp = my_words[ender]
+      my_words[ender] = my_words[start]
+      my_words[start] = temp
+      start += 1
+      ender -= 1
+    end
+  end
+  return my_words
 end
