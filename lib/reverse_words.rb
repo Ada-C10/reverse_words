0c067c5d69e3a83813cfbaf6c0adf1da2f94e796
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-29 08:31:22 -0700

    Write reverse_words with helper method

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,44 @@
 # A method to reverse each word in a sentence, in place.
+def reverse_word(string, beginning, ending)
+
+  i = beginning
+  j = ending
+
+  while j > i
+    temp = string[i]
+    string[i] = string[j]
+    string[j] = temp
+    i += 1
+    j -= 1
+  end
+end
+
 def reverse_words(my_words)
-  raise NotImplementedError
+
+  if  my_words == nil || my_words.length < 2
+    return my_words
+  end
+
+  i = 0
+  j = 0
+
+  while j < my_words.length
+
+    i = j
+
+    while my_words[j] == " "
+      i += 1
+      j += 1
+    end
+
+    while my_words[j] != " " && my_words[j] != nil
+      j += 1
+    end
+
+    reverse_word(my_words, i, j-1)
+
+    j += 1
+  end
+
+  return my_words
 end
